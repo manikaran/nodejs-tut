8381debfc0ee394562c7338601619fdcb9358fd9
Mani Mahal <manikaran10@gmail.com>
2021-06-20 20:11:08 +0530

    first commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
+const _ = require('lodash');
+const items = [1,[2,[3,[4]]]];
 
-const http = require('http');
-const server = http.createServer((req, res) => {
-});
+const newItems = _.flattenDeep(items);
+console.log(newItems);
